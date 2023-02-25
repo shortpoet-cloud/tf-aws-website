3d21ab655d33294fece7cc177f77f3c5a279e680
Carlos Soriano <soriano.carlitos@gmail.com>
2023-02-25 11:11:50 -0500

    add from sp

---
+++ modules/s3_bucket/s3.tf
@@ -0,0 +1,183 @@
+locals {
+  cloudflare_ips = [
+    "173.245.48.0/20",
+    "103.21.244.0/22",
+    "103.22.200.0/22",
+    "103.31.4.0/22",
+    "141.101.64.0/18",
+    "108.162.192.0/18",
+    "190.93.240.0/20",
+    "188.114.96.0/20",
+    "197.234.240.0/22",
+    "198.41.128.0/17",
+    "162.158.0.0/15",
+    "104.16.0.0/12",
+    "172.64.0.0/13",
+    "131.0.72.0/22",
+    "2400:cb00::/32",
+    "2606:4700::/32",
+    "2803:f800::/32",
+    "2405:b500::/32",
+    "2405:8100::/32",
+    "2a06:98c0::/29",
+    "2c0f:f248::/32"
+  ]
+  tags = merge(
+    {
+      Name = var.site_domain_bucket_name
+    },
+    var.tags,
+  )
+}
+
+resource "aws_s3_bucket" "site" {
+  bucket = var.site_domain_bucket_name
+  # acl    = "private"
+  tags = local.tags
+}
+
+resource "aws_s3_bucket_website_configuration" "redirect" {
+  count  = var.redirect_all_requests_to != null ? 1 : 0
+  bucket = aws_s3_bucket.site.id
+
+  redirect_all_requests_to {
+    host_name = var.redirect_all_requests_to
+  }
+}
+
+
+resource "aws_s3_bucket_website_configuration" "site" {
+  bucket = aws_s3_bucket.site.id
+
+  dynamic "redirect_all_requests_to" {
+    for_each = var.redirect_all_requests_to != null ? [var.redirect_all_requests_to] : []
+    content {
+      host_name = redirect_all_requests_to.value
+    }
+  }
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "index.html"
+  }
+
+  # routing_rules = jsonencode([
+  #   {
+  #     Redirect = {
+  #       ReplaceKeyPrefixWith = "/"
+  #       HttpRedirectCode     = "301"
+  #     }
+  #     Condition = {
+  #       KeyPrefixEquals = "index.html"
+  #     }
+  #   },
+  #   {
+  #     Redirect = {
+  #       ReplaceKeyPrefixWith = ""
+  #       HttpRedirectCode     = "301"
+  #     }
+  #     Condition = {
+  #       KeyPrefixEquals = "docs/"
+  #     }
+  #   },
+  # ])
+
+}
+
+resource "aws_s3_bucket_cors_configuration" "example" {
+  bucket = aws_s3_bucket.site.id
+
+  cors_rule {
+    # allowed_headers = ["*"]
+    # allowed_methods = ["PUT", "POST"]
+    # allowed_origins = ["https://s3-website-test.hashicorp.com"]
+    # expose_headers  = ["ETag"]
+    # max_age_seconds = 3000
+    allowed_headers = ["Authorization", "Content-Length"]
+    allowed_methods = ["GET", "POST"]
+    allowed_origins = ["https://www.${var.site_domain_bucket_name}"]
+    max_age_seconds = 3000
+  }
+
+  cors_rule {
+    allowed_methods = ["GET"]
+    allowed_origins = ["*"]
+  }
+}
+
+data "aws_canonical_user_id" "current" {}
+
+resource "aws_s3_bucket_acl" "site" {
+  bucket = aws_s3_bucket.site.id
+
+  # acl = "private"
+  # acl = "public-read"
+
+  access_control_policy {
+    # grant {
+    #   grantee {
+    #     type = "Group"
+    #     uri  = "http://acs.amazonaws.com/groups/global/AllUsers"
+    #   }
+    #   permission = "READ"
+    # }
+
+    grant {
+      grantee {
+        id   = data.aws_canonical_user_id.current.id
+        type = "CanonicalUser"
+      }
+      permission = "FULL_CONTROL"
+    }
+
+    # grant {
+    #   grantee {
+    #     type = "Group"
+    #     uri  = "http://acs.amazonaws.com/groups/s3/LogDelivery"
+    #   }
+    #   permission = "READ_ACP"
+    # }
+
+    owner {
+      id = data.aws_canonical_user_id.current.id
+    }
+  }
+
+
+
+}
+
+resource "aws_s3_bucket_policy" "site" {
+  bucket = aws_s3_bucket.site.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource = [
+          "${aws_s3_bucket.site.arn}/*",
+        ],
+        # Condition = {
+        #   StringEquals = {
+        #     "aws:Referer" = [
+        #       "https://${var.site_domain}/*",
+        #       "https://${var.site_domain}",
+        #     ]
+        #   }
+        # }
+        Condition = {
+          IpAddress = {
+            "aws:SourceIp" = local.cloudflare_ips
+          }
+        }
+      },
+    ]
+  })
+}
